632b8fb59ddcfbd1b109a7ffaa296e5914cfbe3e
nicoargiolas <argiolas.nicola00@gmail.com>
2025-06-06 11:29:52 +0200

    Bonus 2

--- script.js
+++ script.js
@@ -100,18 +100,78 @@ async function fetchJson(url) {
 // Vienna is in Austria.
 // The main airport is Vienna International Airport.
 
+// async function getDashboardData(query) {
+//     const promises = [
+//         fetchJson(`http://localhost:3333/destinations?search=${query}`),
+//         fetchJson(`http://localhost:3333/weathers?search=${query}`),
+//         fetchJson(`http://localhost:3333/airports?search=${query}`),
+//     ];
+//     const allData = await Promise.all(promises);
+
+//     function checkData(array, propertyName) {
+//         let result = null;
+//         if (array.length > 0) {
+//             result = array[0][propertyName];
+//         }
+//         return result;
+//     }
+
+//     const data = {
+//         'city': checkData(allData[0], 'name'),
+//         'country': checkData(allData[0], 'country'),
+//         'temperature': checkData(allData[1], 'temperature'),
+//         'weather': checkData(allData[1], 'weather_description'),
+//         'airport': checkData(allData[2], 'name')
+//     }
+
+//     return data;
+// }
+
+// (async () => {
+//     try {
+//         const data = await getDashboardData('vienna');
+//         let message = ``;
+//         if (data.city !== null && data.country !== null) {
+//             message += `${data.city} is in ${data.country}.\n`
+//         }
+//         if (data.temperature !== null && data.weather !== null) {
+//             message += `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`
+//         }
+//         if (data.airport !== null) {
+//             message += `The main airport is ${data.airport}.\n`
+//         }
+//         console.log('Dasboard data:', data);
+//         console.log(message);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// })()
+
+
+
+// 🎯 Bonus 2 - Chiamate fallite
+
+// Attualmente, se una delle chiamate fallisce, **Promise.all()** rigetta l'intera operazione.
+
+// Modifica `getDashboardData()` per usare **Promise.allSettled()**, in modo che:
+// Se una chiamata fallisce, i dati relativi a quella chiamata verranno settati a null.
+// Stampa in console un messaggio di errore per ogni richiesta fallita.
+// Testa la funzione con un link fittizio per il meteo (es. https://www.meteofittizio.it).
+
 async function getDashboardData(query) {
     const promises = [
         fetchJson(`http://localhost:3333/destinations?search=${query}`),
-        fetchJson(`http://localhost:3333/weathers?search=${query}`),
-        fetchJson(`http://localhost:3333/airports?search=${query}`),
+        fetchJson(`https://www.meteofittizio.it`),
+        fetchJson(`https://www.aeroportofittizio.it`),
     ];
-    const allData = await Promise.all(promises);
+    const allData = await Promise.allSettled(promises);
 
     function checkData(array, propertyName) {
         let result = null;
-        if (array.length > 0) {
-            result = array[0][propertyName];
+        if (array.status === 'fulfilled') {
+            if (array.value.length > 0) {
+                result = array.value[0][propertyName];
+            }
         }
         return result;
     }
@@ -146,14 +206,3 @@ async function getDashboardData(query) {
         console.error(error);
     }
 })()
-
-
-
-// 🎯 Bonus 2 - Chiamate fallite
-
-// Attualmente, se una delle chiamate fallisce, **Promise.all()** rigetta l'intera operazione.
-
-// Modifica `getDashboardData()` per usare **Promise.allSettled()**, in modo che:
-// Se una chiamata fallisce, i dati relativi a quella chiamata verranno settati a null.
-// Stampa in console un messaggio di errore per ogni richiesta fallita.
-// Testa la funzione con un link fittizio per il meteo (es. https://www.meteofittizio.it).
